9f27c688e2c89a6e0299820cb73c2f5475c9cd05
Alex Encinas <alexencmer@gmail.com>
2023-07-04 20:31:31 +0200

    buscador de pokemon

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,5 +1,5 @@
 import styles from "./PokemonList.module.css";
-import React from "react";
+import React, { FC } from "react";
 import Link from "next/link";
 
 export const PokemonList: React.FC<{ pokemons: any[] }> = ({ pokemons }) => {
@@ -8,19 +8,42 @@ export const PokemonList: React.FC<{ pokemons: any[] }> = ({ pokemons }) => {
       <h1>Lista de todos los pokemon</h1>
       <div className={styles.pokemonListContainer}>
         {pokemons.map((pokemon: any, i) => (
-          <Link href={`/pokemon/${i + 1}`} passHref>
-            <div className={styles.pokemonCard}>
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                  i + 1
-                }.png`}
-                alt=""
-              />
-              <p>{pokemon.name}</p>
-            </div>
-          </Link>
+          <PokemonCard pokemon={pokemon} i={i} />
         ))}
       </div>
     </div>
+  );
+};
+
+export const PokemonCard: FC<{
+  pokemon: any;
+  i?: number;
+  isFullPokemon?: boolean;
+}> = ({ pokemon, i, isFullPokemon = false }) => {
+  if (!isFullPokemon)
+    return (
+      <Link href={`/pokemon/${i! + 1}`} passHref>
+        <div className={styles.pokemonCard}>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              i! + 1
+            }.png`}
+            alt=""
+          />
+          <p>{pokemon.name}</p>
+        </div>
+      </Link>
+    );
+
+  return (
+    <Link href={`/pokemon/${pokemon.id}`} passHref>
+      <div className={styles.pokemonCard}>
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+          alt=""
+        />
+        <p>{pokemon.name}</p>
+      </div>
+    </Link>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,57 @@
 import Navbar from "@/components/Navbar";
 import styles from "../styles/Home.module.css";
-import { PokemonList } from "@/components/PokemonList";
+import { PokemonCard, PokemonList } from "@/components/PokemonList";
 import axios from "axios";
 
 export default function Home(ctx: any) {
-  console.log(ctx);
+  const [input, setInput] = useState("");
+  const [searchPokemonInfo, setSearchPokemonInfo] = useState<any>(null);
+  const [error, setError] = useState(false);
+
+  const getPokemonByName = async (name: string) => {
+    if (name.length <= 2) {
+      setSearchPokemonInfo(null);
+      setError(false);
+    }
+    try {
+      const { data } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${name}`
+      );
+      setSearchPokemonInfo(data);
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setSearchPokemonInfo(null);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    // llamar a la api de pokemon por el nombre del input
+    // si existe un pokemon con ese nombre renderizarlo
+    // si no, mostrar un error de que no existe o algo ha salido mal
+    getPokemonByName(input);
+  }, [input]);
+
   return (
     <>
       <Navbar numeros={ctx.randomNumbers} />
+      <div>
+        <h3>Encuentra a tu pokemon favorio:</h3>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        {error && <p className={styles.errorText}>Ese pokemon no existe</p>}
+        {searchPokemonInfo && input.length > 2 && (
+          <PokemonCard pokemon={searchPokemonInfo} isFullPokemon />
+        )}
+      </div>
       <PokemonList pokemons={ctx.pokemonList} />
       <h1 className={styles.title}>Pagina principal</h1>
     </>
@@ -18,6 +62,7 @@ export default function Home(ctx: any) {
 // pokemon aleatorios del navbar
 // nextssr
 import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { data } = await axios.get(
